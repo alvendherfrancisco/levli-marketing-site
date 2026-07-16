b358d75ca8045d3cb172fb5f532bc30554bc2dcc
Alvendher Joy Francisco <alvendherfrancisco01@gmail.com>
2026-07-16 18:46:42 +0000

    Visual edits

--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -12,7 +12,7 @@ const people = [
 {
   name: 'Nadia',
   role: 'Mounjaro user',
-  avatar: "https://media.base44.com/images/public/6a58db45b04e79060109b350/9f7a1b037_Gemini_Generated_Image_68e93768e93768e9.png",
+  avatar: "https://media.base44.com/images/public/6a58db45b04e79060109b350/5ca4df9ca_gemini_6.png",
   contain: true,
   quote: 'I stopped obsessing over the scale. Levli helps me understand what is happening and feel calmer about it.'
 },

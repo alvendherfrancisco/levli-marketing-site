b80a53cf2cdff7f22ad9fddcc05726fa4cce4d6b
Alvendher Joy Francisco <alvendherfrancisco01@gmail.com>
2026-07-16 18:39:26 +0000

    Visual edits

--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -25,7 +25,7 @@ const people = [
 {
   name: 'Aisha',
   role: 'Zepbound user',
-  avatar: "https://media.base44.com/images/public/6a58db45b04e79060109b350/a19bfd159_1_gemini.png",
+  avatar: "https://media.base44.com/images/public/6a58db45b04e79060109b350/50280f928_2_gemini.png",
   quote: 'Tracking my symptoms next to my shots showed me patterns I never noticed. I feel so much more prepared.'
 },
 {
